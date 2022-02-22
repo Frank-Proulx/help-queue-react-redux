d13690bed26128237de74d15179bedce48b75dad
Frank-Proulx <frankproulx7@gmail.com>
2022-02-22 08:00:18 -0800

    add authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.png";
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
-      <img src={ticketsImage} alt="An image of tickets" />
+      <h1> Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
